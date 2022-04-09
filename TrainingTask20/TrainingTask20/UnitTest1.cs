73fa89b1aee3f5fc2249c156ceb398e07ab009a8
Oksana Levkovskaya <oksizhura@gmail.com>
2022-04-09 18:04:04 +0300

    final commit

--- TrainingTask20/TrainingTask20/UnitTest1.cs
+++ TrainingTask20/TrainingTask20/UnitTest1.cs
@@ -34,7 +34,7 @@ namespace TrainingTask20
             signInButton.Click();
             Thread.Sleep(2000);
             var userLabelDisplayed = driver.FindElement(By.ClassName("desk-notif-card__title")).Text;
-            var userLoggedInLabel = "mastermister1233";
+            var userLoggedInLabel = "mastermister123";
 
             Assert.AreEqual(userLabelDisplayed, userLoggedInLabel, "User Label is not found on page.");
                 
